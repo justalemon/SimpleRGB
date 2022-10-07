d0ffc7c33b208c44e980c7d4e427a9fa4b634e02
Hannele Ruiz <justlemoncl@gmail.com>
2022-10-06 21:02:49 -0300

    Added simple notification icon

--- SimpleRGB/Program.cs
+++ SimpleRGB/Program.cs
@@ -1,7 +1,43 @@
 using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace SimpleRGB;
+
+/// <summary>
+/// The custom Windows Forms context.
+/// </summary>
+public class Context : ApplicationContext
+{
+    private readonly NotifyIcon notifyIcon;
+    private readonly ContextMenuStrip strip;
+    private readonly Icon icon;
+    
+    /// <summary>
+    /// Creates a new custom Context.
+    /// </summary>
+    public Context()
+    {
+        icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+        strip = new ContextMenuStrip();
+        notifyIcon = new NotifyIcon();
+
+        notifyIcon.Icon = icon;
+        notifyIcon.ContextMenuStrip = strip;
+        notifyIcon.Visible = true;
+        
+        ThreadExit += OnThreadExit;
+    }
+
+    private void OnThreadExit(object sender, EventArgs e)
+    {
+        notifyIcon.Dispose();
+        strip.Dispose();
+        icon.Dispose();
+    }
+}
 
 /// <summary>
 /// The program basics.
@@ -15,6 +51,6 @@ public static class Program
     public static void Main()
     {
         ApplicationConfiguration.Initialize();
-        // Application.Run();
+        Application.Run(new Context());
     }
 }
